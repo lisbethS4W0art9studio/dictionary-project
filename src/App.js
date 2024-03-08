fefe290757e6a2bacb0b814ec0667153607bde03
Lisbeth Stafferton <131831523+lisbethS4W0art9studio@users.noreply.github.com>
2024-03-08 13:32:12 +1100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./b and w dictionary.png";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
