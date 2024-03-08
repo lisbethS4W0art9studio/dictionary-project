4fe29dcf76c71f2c72927d5e1c0ddff43438f445
Lisbeth Stafferton <131831523+lisbethS4W0art9studio@users.noreply.github.com>
2024-03-08 14:42:25 +1100

    Created the form of the search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import logo from "./b and w dictionary.png";
 import "./App.css";
-
+import Dictionary from "./Dictionary";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Lisbeth</footer>
+      </div>
     </div>
   );
 }
